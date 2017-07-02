69b43016158174104143204695207464a5a6b57a
Osyo <819705364@qq.com>
2017-07-03 02:05:10 +0800

    初始化，读取并处理卫星数据

---
+++ GPS/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+namespace GPS
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<N> nFile = new List<N>();
+        fileReader fileReader = new fileReader();
+        string[] GPS;
+
+        // GPS位置
+        private void positionOfGPS(object sender, RoutedEventArgs e)
+        {
+        }
+        // 接收机坐标
+        private void positionOfReceiver(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // 基线长度
+        private void lengthOfBaseline(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // 读取O文件
+        private void readO(object sender, RoutedEventArgs e)
+        {
+            List<List<string>> O = fileReader.readFile(10);
+            // 要用到的卫星
+            string[] GPS = O[0][7].Split('G');
+        }
+        // 读取N文件
+        private void readN(object sender, RoutedEventArgs e)
+        {
+            
+            List<List<string>> N0 = fileReader.readFile(8, true);
+            N0.ForEach(item =>
+            {
+
+            });
+        }
+    }
+
+    // 广播星历数据结构
+    public class N
+    {
+        public static int PRN, TOC, year, month, day, hour, min;
+        public static float sec, clockSkew, clockDrift, clockDriftSpeed, IODE, Crs, 
+                     delta_n, M0, Cuc, e, Cus, sqrt_A, TOE, Cic, OMEGA0, Cis, 
+                     i0, Crc, omega, OMEGA, i, L2, GPSWeek, L2P, satellitePrecision, 
+                     satelliteHealth, TGD, IODC, sentTime, fittingRange;
+
+        public N(string[] data)
+        {
+            int PRN = ConvertTo<int>(data[0]);
+        }
+        
+        // 数据类型转换
+        public T ConvertTo<T>(string val) where T : struct
+        {
+            return (T)Convert.ChangeType(val, Type.GetTypeCode(typeof(T)));
+        }
+
+        // GPS位置
+        const double MU = 3.986005E+14;
+        public double[] position()
+        {
+            double n = Math.Sqrt(MU) / Math.Pow(sqrt_A, 3) + delta_n;
+            double tk = 0;
+            double Mk = M0 + n * tk;
+            double Ek = Mk;
+            Ek = Mk + e * Math.Sin(Ek);
+            Ek = Mk + e * Math.Sin(Ek);
+            double fk = Math.Atan(Math.Sqrt(1 - e * e) * Math.Sin(Ek) / (Math.Cos(Ek) - e));
+            double PHik = fk + omega;
+            double deltau = delta(Cuc, Cus);
+            double deltar = delta(Crc, Crs);
+            double deltai = delta(Cic, Cis);
+            double delta(double Cxc, double Cxs)
+            {
+                return Cxc * Math.Cos(2 * PHik) + Cxs * Math.Sin(2 * PHik);
+            }
+            double uk = PHik + deltau;
+            double rk = sqrt_A * sqrt_A * (1 - e * Math.Cos(Ek)) + deltar;
+            double ik = i0 + deltai + i * tk;
+            double xk = rk * Math.Cos(uk);
+            double yk = rk * Math.Sin(uk);
+
+
+            double[] position = new double[3];
+            return position;
+        }
+
+
+    }
+}
---
+++ GPS/fileReader.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GPS
+{
+    class fileReader
+    {
+        // 导入文件
+        public List<List<string>> readFile(int count, bool start = false)
+        {
+            List<List<string>> dataChunk = new List<List<string>>();
+            Stream myStream = null;
+            OpenFileDialog f = new OpenFileDialog();
+
+            f.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            f.FilterIndex = 2;
+            f.RestoreDirectory = true;
+
+            if (f.ShowDialog() == true)
+            {
+                try
+                {
+                    if ((myStream = f.OpenFile()) != null)
+                    {
+                        using (StreamReader r = new StreamReader(f.OpenFile()))
+                        {
+                            string line;
+                            List<string> chunk = new List<string>();
+                            int index = 0;
+                            while ((line = r.ReadLine()) != null)
+                            {
+                                if (start)
+                                {
+                                    if (line.IndexOf('D') != -1) line = addSpace(line);
+                                    List<string> tempArr = line.Split(' ').ToList();
+                                    tempArr.RemoveAll(String.IsNullOrEmpty);
+                                    for (int i = 0; i < tempArr.Count; ++i)
+                                    {
+                                        if (tempArr[i].IndexOf('D') != -1)
+                                        {
+                                            tempArr[i] = toNumber(tempArr[i]);
+                                        }
+                                        chunk.Add(tempArr[i]);
+                                    }
+                                    if (++index >= count)
+                                    {
+                                        index = 0;
+                                        dataChunk.Add(chunk);
+                                        chunk = new List<string>();
+                                    }
+                                }
+                                // 去掉头
+                                if (line.Trim() == "END OF HEADER")
+                                    start = true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            return dataChunk;
+        }
+
+        // 在－前面加空格
+        private string addSpace(string line)
+        {
+            int index = 20;
+            while ((index = line.IndexOf('-', index)) != -1)
+            {
+                if (line[index - 1] != 'D') line = line.Insert(index, " ");
+                index += 2;
+            }
+            return line;
+        }
+
+        // 科学计数法转换成浮点数
+        private string toNumber(string str)
+        {
+            str = str.Replace('D', 'E');
+            int indexD = str.IndexOf('E');
+            int exponent;
+            int lastIndex = str.LastIndexOf('-');
+            int dec = lastIndex == -1 || lastIndex == 0 ? str.LastIndexOf('+') : str.LastIndexOf('-');
+            if ((exponent = Convert.ToInt32(str.Substring(dec + 1))) == 0) return str.Substring(0, indexD);
+            int count = indexD - str.IndexOf('.') - 1 + exponent;
+            char[] decimals = new char[count];
+            for (int i = 0; i < count; ++i)
+            {
+                decimals[i] = '#';
+            }
+            string format = "0." + string.Join("", decimals);
+            str = Double.Parse(str, System.Globalization.NumberStyles.Float).ToString(format);
+            return str;
+        }
+    }
+}
